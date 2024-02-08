fcc6282c72c216680d85c0a2abd9cb05b6ea44d8
Chance <chancemyoung@gmail.com>
2024-02-07 19:49:31 -0800

    Added icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { UserNav, SiteNav } from './nav'}
+import { UserNav, SiteNav } from './nav'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
